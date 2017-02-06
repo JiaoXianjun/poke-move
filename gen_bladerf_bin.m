6a08256932520da9a367f3e8765d96853da5640c
Xianjun Jiao <xianjun.jiao@intec.ugent.be>
2017-02-06 20:33:34 +0100

    update

--- gen_bladerf_bin.m
+++ gen_bladerf_bin.m
@@ -1,4 +1,4 @@
-function gen_bladerf_bin(gps_file,lat_lon_poi_filename, len_second)
+function gen_bladerf_bin(gps_file,lat_lon_poi_filename, start_h_m, len_second)
 % len_second at least 40s
 
 start_minute = 0;
@@ -28,37 +28,50 @@ time_vec = clock;
 %time_vec(1) = 2017;
 %time_vec(2) = 1;
 %time_vec(3) = 29;
-h = time_vec(4);
-m = time_vec(5);
+%h = time_vec(4);
+%m = time_vec(5);
+h = start_h_m(1);
+m = start_h_m(2);
 s = floor(time_vec(6));
-m_total = h*60+m;
-m_total = m_total - 120;
-h = floor(m_total/60);
-m = m_total - h*60;
-time_str = [num2str(time_vec(1)) '/' num2str(time_vec(2)) '/' num2str(time_vec(3)) ',' num2str(h) ':' num2str(m) ':' num2str(s)];
+%m_total = h*60+m;
+%m_total = m_total - 120;
+%h = floor(m_total/60);
+%m = m_total - h*60;
 
-for i=1:size(lat_lon_poi,1)
-  lat = lat_lon_poi(i,1);
-  lon = lat_lon_poi(i,2);
-  bin_filename = ['gps_' num2str(lat) '_' num2str(lon) '_' num2str(len_second) 's_bladerf.bin'];
-  script_filename = ['gps_' num2str(lat) '_' num2str(lon) '_' num2str(len_second) 's_bladerf.script'];
-  disp([bin_filename ' ' script_filename]);
-  
-  % generation bladerf bin and script files
-  bin_gen_str = ['./gps-sdr-sim -e ' gps_file ' -l ' num2str(lat) ',' num2str(lon) ',8 -t ' time_str ...
-  ' -o ' bin_filename ' -d ' num2str(len_second)];
-
-  disp(bin_gen_str); fflush(1);
-
-  system(bin_gen_str);
-  
-  fid = fopen(script_filename,'w');
-  if fid == -1
-    disp('fopen script_filename fail.');
-    return;
-  end
-  fprintf(fid, 'set frequency tx 1575.42M\nset samplerate 2.6M\nset bandwidth 2.5M\nset txvga1 -10\nset txvga2 0\ncal lms\ncal dc tx\ntx config file=%s format=bin\ntx start\ntx wait\n', bin_filename);
-  fclose(fid);
-
-  start_minute = start_minute + 1;
+script_filename = ['gps_' num2str(len_second) 's_bladerf_all.script'];
+fid = fopen(script_filename,'w');
+if fid == -1
+  disp('fopen script_filename fail.');
+  return;
 end
+
+start_minute = m;
+for j=1:3
+  for i=1:size(lat_lon_poi,1)
+    lat = lat_lon_poi(i,1);
+    lon = lat_lon_poi(i,2);
+    bin_filename = ['gps_' num2str(lat,"%12.8f") '_' num2str(lon,"%12.8f") '_' num2str(len_second) 's_bladerf.bin'];
+    disp([bin_filename ' ' script_filename]);
+    
+    if isempty(dir(bin_filename))
+        disp('bladerf bin file NOT FOUND. generating...'); fflush(1);
+        % generation bladerf bin and script files
+        time_str = [num2str(time_vec(1)) '/' num2str(time_vec(2)) '/' num2str(time_vec(3)) ',' num2str(h) ':' num2str(start_minute) ':' num2str(s)];
+        bin_gen_str = ['./gps-sdr-sim -e ' gps_file ' -l ' num2str(lat) ',' num2str(lon) ',8 -t ' time_str ...
+        ' -o ' bin_filename ' -d ' num2str(len_second)];
+
+        disp(bin_gen_str); fflush(1);
+
+        system(bin_gen_str);
+    else
+        disp('bladerf bin file     FOUND.'); fflush(1);
+    end
+    
+    fprintf(fid, 'set frequency tx 1575.42M\nset samplerate 2.6M\nset bandwidth 2.5M\nset txvga1 -20\nset txvga2 0\ncal lms\ncal dc tx\ntx config file=%s format=bin\ntx start\ntx wait\n', bin_filename);
+
+    start_minute = start_minute + 1;
+  end
+end
+
+fclose(fid);
+
